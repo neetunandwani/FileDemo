e5649e39c9fa3a020732a7dd73300a44b1d73f7b
Neetu Nandwani <neetu.nandwani@tothenew.com>
2019-05-09 20:44:05 +0530

    Revert "test"
    This reverts commit ea083e57fcd334868e0178e5531fca875ee36b79.

--- src/main/java/com/test/parse/CO.java
+++
@@ -1,4 +0,0 @@
-package com.test.parse;
-
-public interface CO {
-}
--- src/main/java/com/test/parse/DataSource.java
+++
@@ -1,8 +0,0 @@
-package com.test.parse;
-
-import java.io.FileNotFoundException;
-import java.util.List;
-
-public interface DataSource<T> {
-    List<T> getData(Class<T> clazz) throws FileNotFoundException;
-}
